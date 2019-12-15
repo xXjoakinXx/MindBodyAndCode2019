8b5bd85c2b6600610d2226167cf790d590150f3b
Joaquin Garcia Piñero <joaquin.garcia@orenesgrupo.com>
2019-12-15 21:45:20 +0100

    feat: authorize Actor

---
+++ ReactiveAkkaAPI/ReactiveAkkaAPI/Actors/SystemReceptionist.cs
@@ -0,0 +1,31 @@
+﻿using Akka.Actor;
+using ReactiveAkkaAPI.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactiveAkkaAPI.Actors
+{
+    public class SystemReceptionist : ReceiveActor
+    {
+        public SystemReceptionist()
+        {
+            Receive<AuthenticatedMessageEnvelope>(env =>
+            {
+                if (IsAuthorised(env.AuthenticationToken)) // Se valida el token y si es valido...
+                {
+                    env.TargetActor.Forward(env.Payload); // Forward del payload del mensaje. Es decir se manda el mensaje en si 
+                                                         // la diferencia con el Send es que el emisor es el originario. Para el receptor
+                                                         // este actor SystemReceptionist es totalmente desconocido.
+                }
+            });
+        }
+
+        private bool IsAuthorised(string authToken)
+        {
+            // Code to authorizate token
+            return true;
+        }
+    }
+}
---
+++ ReactiveAkkaAPI/ReactiveAkkaAPI/Messages/AuthenticatedMessageEnvelope.cs
@@ -0,0 +1,28 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactiveAkkaAPI.Messages
+{
+    public class AuthenticatedMessageEnvelope
+    {
+        private readonly object _payload;
+        private readonly IActorRef _targetActor;
+        private readonly string _authenticationToken;
+
+        public object Payload => _payload; 
+        public IActorRef TargetActor => _targetActor;
+        public string AuthenticationToken => _authenticationToken;
+
+        public AuthenticatedMessageEnvelope(object payload, IActorRef targetActor, string authenticationToken)
+        {
+            _payload = payload;
+            _targetActor = targetActor;
+            _authenticationToken = authenticationToken;
+        }
+
+
+    }
+}
